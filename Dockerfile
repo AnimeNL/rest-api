47306a322dfd2ac8f19bf2d482dd5c67ec8417f3
Ian Hulsbus <ian@hulsbus.be>
2021-08-29 15:45:39 +0200

    update to actual working API

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,10 @@ FROM php:7.4-fpm-alpine3.12
 RUN apk update && \
     apk add --no-cache wget nginx
 
-COPY . /var/www/rest/.
+COPY . /var/www/html/.
 
-WORKDIR /var/www/rest
-RUN chmod +x /var/www/rest/entrypoint.sh && \
+WORKDIR /var/www/html
+RUN chmod +x /var/www/html/entrypoint.sh && \
     wget https://getcomposer.org/download/1.10.19/composer.phar && \
     php composer.phar install && \
     rm /etc/nginx/conf.d/default.conf && \
@@ -14,4 +14,4 @@ RUN chmod +x /var/www/rest/entrypoint.sh && \
 
 RUN apk del --purge wget 
 
-ENTRYPOINT ["/var/www/rest/entrypoint.sh"]
+ENTRYPOINT ["/var/www/html/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,26 +3,36 @@
 # create nginx config
 /bin/cat <<'EOF' > /etc/nginx/conf.d/rest.conf
 server {
-    index index.php index.html;
-    server_name php-docker.local;
+    index index.php;
+    server_name rest_api.local;
     error_log  /var/log/nginx/error.log;
     access_log /var/log/nginx/access.log;
-    root /var/www/rest/public;
+    root /var/www/html/public;
 
-    location ~ \.php$ {
+    location / {
+        # try to serve file directly, fallback to index.php
+        try_files $uri /index.php$is_args$args;
+    }
+
+    location ~ ^/index\.php(/|$) {
         try_files $uri =404;
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
         fastcgi_pass localhost:9000; 
-        fastcgi_index index.php;
+        fastcgi_split_path_info ^(.+\.php)(/.*)$;
         include fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        fastcgi_param PATH_INFO $fastcgi_path_info;
+        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
+        fastcgi_param DOCUMENT_ROOT $realpath_root;
+        internal;
+    }
+    
+    location ~ \.php$ {
+        return 404;
     }
 }
 EOF
 
 # Set work dir
-cd /var/www/rest
+cd /var/www/html
 
 echo "Starting rest server"
 php-fpm -D; nginx -g 'daemon off;'
